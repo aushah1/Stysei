ddeecef4f7abf5e8606314fb8a6e03ac3126b2dc
Aushah Gowhar <waniaushahgowhar@gmail.com>
2025-12-18 23:22:41 +0530

    added port

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const isLoggedin = require("./middleware/isLoggedin");
 
 require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose
   .connect(process.env.MONGO_URI)
